a960ef0aeb38fc9c5cc2cbb2ac10b206e6190461
Russell Purinton <russell.purinton@gmail.com>
2025-04-07 17:38:51 +0000

    2025-04-07 17:38:51

--- src/Research.php
+++ src/Research.php
@@ -11,9 +11,26 @@ class Research
 
     public function __construct(array $argv = [])
     {
-        $this->ai = OpenAI::connect();
 	unset($argv[0]);
         $this->query = implode(" ", $argv);
-        echo("Query: {$this->query}\n");
+        if(empty($this->query)) die("Usage: research <query>\n");
+        $this->ai = OpenAI::connect(getenv('OPENAI_KEY_RESEARCH'));
+        $this->ai->prompt['messages'] = [
+            [
+                'role' => 'system',
+                'content' => file_get_contents(__DIR__ . '/../prompt/system.txt'),
+            ]
+        ];
+    }
+
+    public static function begin(array $argv = [])
+    {
+        (new self($argv))->run();
+    }
+
+    public function run()
+    {
+        $response = $this->ai->ask("User research request: {$this->query}");
+        print_r($response);
     }
 }
